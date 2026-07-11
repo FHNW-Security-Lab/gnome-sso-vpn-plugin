9ce4e2bb030a6e2db5d93ec0de787ac946fca3f7
Christopher Scherb <christopher.scherb@fhnw.ch>
2026-07-11 12:28:19 +0200

    Release version 2.0.2

--- build-deb.sh
+++ build-deb.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PACKAGE_NAME="network-manager-ms-sso"
-VERSION="2.0.1"
+VERSION="2.0.2"
 
 BUILD_DEPENDENCIES=(
     dpkg-dev
--- nix/networkmanager-ms-sso.nix
+++ nix/networkmanager-ms-sso.nix
@@ -40,7 +40,7 @@ let
 in
 python3Packages.buildPythonApplication rec {
   pname = "networkmanager-ms-sso";
-  version = "2.0.1";
+  version = "2.0.2";
   format = "other";
 
   src = lib.cleanSource ../.;
